6e5bc13f552b4a6c17e48dd0b97d9f8e4813bc2b
Alfonso Rafael <alfonsorafaelcamacho@gmail.com>
2025-09-21 09:46:30 -0600

    Implementando una funcion para sortear los amigos

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 let elementoLista = document.getElementById('listaAmigos');
+let resulatoAmigo = document.getElementById('resultado');
 
-
-
+//Funcion que hace funcionar el boton añadir
 function agregarAmigo(){    
     let amigo = document.getElementById('amigo').value; //Se obtiene el valor que se ingresa en el input.
     if(amigo == ""){
@@ -30,6 +30,27 @@ function recorreListaAmigos(){
         nuevoItemDeLista.textContent = amigos[i];
         elementoLista.appendChild(nuevoItemDeLista);
     } 
+}
+
+//Funcion que habilita el sorteo de los amigos
+let indiceAleatorio;
+let nombreSeleccionado;
+function sortearAmigo(){
+    if(amigos.length != 0){
+        //Genera el indice aleatorio para indicar que amigo sale sorteado
+        indiceAleatorio = parseInt(Math.floor(Math.random()*amigos.length)+1);
+        console.log(indiceAleatorio);
+        //Se selecciona el nombre y se crea un mensaje
+        nombreSeleccionado = document.createElement('li');
+        nombreSeleccionado.textContent = `El amigo secreto sorteado es: ${amigos[indiceAleatorio]}`;
+        //Limpia la lista de amigos
+        elementoLista.innerHTML = "";
+        //Se muestra el mensaje del amigo sorteado
+        resulatoAmigo.appendChild(nombreSeleccionado);
+
+    }else{
+        alert("Debes ingresar al menos a una persona en el campo 'Escribe un nombre'");
+    }
 }
 
 
